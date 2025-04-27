42efcc60056d969230fd4993119bb605b0b00e3c
Andre Buchmann <androzo@gmail.com>
2025-04-27 11:28:21 -0300

    test with wrong team

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "team_bucket" {
 
   tags = {
     Name = "${var.team_name}-${var.environment}-bucket"
-    # team        = var.team_name
-    # environment = var.environment
+    team        = "not-my-team-name"
+    environment = var.environment
   }
 }
